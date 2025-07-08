55c57ac75aaa05adcd7bdcbf9aab53b893db59ad
wrealaero <clanwarobi@gmail.com>
2025-07-08 15:49:39 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 repeat task.wait() until game:IsLoaded()
 if shared.vape then shared.vape:Uninject() end
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wrealaero/NewAeroV4/refs/heads/main/Anti-Crash.lua", true))()
+
 if identifyexecutor then
 	if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
 		getgenv().setthreadidentity = nil
